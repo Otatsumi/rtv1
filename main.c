08d380ee8eeffdb1ab5c8568eeb8a114a67faea0
bauwen_j <bauwen_j@epitech.eu>
2014-03-13 18:57:03 +0000

    start rt

--- main.c
+++ main.c
@@ -8,9 +8,29 @@
 ** Last update Mon Mar 10 12:24:53 2014 bauwen_j
 */
 
+#include <stdlib.h>
 #include <mlx.h>
+#include "my.h"
 
-int	main()
+int	gere_key(int keycode)
 {
+  if (keycode == 65307)
+    exit(EXIT_SUCCESS);
+  return (keycode);
+}
+
+int		main()
+{
+  t_wmlx	mlx;
+
+  if ((mlx.mlx = mlx_init()) == 0)
+    return (-1);
+  if ((mlx.win = mlx_new_window(mlx.mlx, SIZE_X, SIZE_Y, "Hello world!")) == 0)
+    return (-1);
+  mlx.img = mlx_new_image(mlx.mlx, SIZE_X, SIZE_Y);
+  /* mlx.data = mlx_get_data_addr(mlx.img, mlx.val.bpp, mlx.val.line, mlx.val.endian); */
+  mlx_put_image_to_window(mlx.mlx, mlx.win, mlx.img, 0, 0);
+  mlx_key_hook(mlx.win, &gere_key, &mlx);
+  mlx_loop(mlx.mlx);
   return (0);
 }
