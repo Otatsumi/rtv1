36961cc2cb9749528a08d3ee9549e8331b2ae4d5
bauwen_j <bauwen_j@epitech.eu>
2014-03-14 14:17:29 +0000

    start form

---
+++ form.c
@@ -0,0 +1,29 @@
+/*
+** form.c for  in /home/bauwen_j/rendu/my_git/rtv1
+** 
+** Made by bauwen_j
+** Login   <bauwen_j@epitech.eu>
+** 
+** Started on  Fri Mar 14 14:08:16 2014 bauwen_j
+** Last update Fri Mar 14 14:08:16 2014 bauwen_j
+*/
+
+#include <stdlib>
+#include "my.h"
+
+char	*my_cercle()
+{
+  return (NULL);
+}
+
+char	*my_plan(t_wmlx mlx)
+{
+  int	i;
+
+  i = (SIZE_X / 4) + SIZE_Y / 2 * (mlx.val.bpp / 8);
+  while (i < SIZE_X * SIZE_Y * (mlx.val.bpp / 8))
+    {
+      
+    }
+
+}
